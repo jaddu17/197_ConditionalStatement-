542e348878d5079f8bf4686c61928fd5ab994231
jaddu17 <shafajaddu@gmail.com>
2024-03-05 11:14:39 +0700

    Add project files.

---
+++ Pertemuan2/Pertemuan2.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int bilangan;
+    srand(time(0));
+
+    bilangan = rand() % 10;
+    
+    cout << "Bilangan awal =" << bilangan << endl;
+
+    if (bilangan >= 5) {
+        bilangan = 2 * bilangan;
+
+    }
+
+    cout << "bilangan akhir = " << bilangan << endl;
+    return 0;
+    
+}
+
+
+
